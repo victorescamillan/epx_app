bf273cf0d518cae741291413809cb2cc71bcb4b8
thor <victor.e@ideahubsolutionsinc.com>
2018-06-20 14:46:42 +0800

    fix private chat issue

--- README.md
+++
@@ -1,1 +0,0 @@
-# raps_mobile
--- src/pipes/pipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SearchMembersPipe } from './search-members/search-members';
-
-@NgModule({
-	declarations: [SearchMembersPipe],
-	imports: [],
-	exports: [SearchMembersPipe]
-})
-export class PipesModule {}
